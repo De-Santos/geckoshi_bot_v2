5cb1117dc18ed7a8704dbd56e9c8d5e27667e008
Misha <miha.18012006@gmail.com>
2024-08-23 00:07:28 +0200

    add some env configurations for sqlalchemy engine

--- database/conf.py
+++ database/conf.py
@@ -7,7 +7,12 @@ from sqlalchemy.orm import sessionmaker, Session, scoped_session
 
 load_dotenv()
 
-engine = create_engine(os.getenv('DATABASE_URL'), echo=False)
+engine = create_engine(os.getenv('DATABASE_URL'),
+                       pool_size=int(os.getenv('DB_ENGINE_POOL_SIZE')),
+                       max_overflow=int(os.getenv('DB_ENGINE_MAX_OVERFLOW')),
+                       pool_pre_ping=True,
+                       pool_recycle=1800,
+                       echo=bool(int(os.getenv('DB_ENGINE_ECHO'))))
 SessionFactory = sessionmaker(bind=engine, autocommit=False, autoflush=False, expire_on_commit=False)
 ScopedSession = scoped_session(SessionFactory)
 Base = declarative_base()
