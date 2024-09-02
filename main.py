49d03982b6502ef427d128652a25c3d5108765c1
Misha <miha.18012006@gmail.com>
2024-09-02 10:17:28 +0200

    remove duplicate

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import logging
-
 import bot_starter
-from variables import stdout_handler, stderr_handler, MODE
+from variables import MODE
 
 if __name__ == '__main__':
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(asctime)s - %(levelname)s - %(name)s - %(message)s',
-        handlers=[stdout_handler, stderr_handler]
-    )
     bot_starter.start(MODE)
+
